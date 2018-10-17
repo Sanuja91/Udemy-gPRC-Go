711c9a2eca729aadd86f18e2d48f2749ee5e2424
Sanuja91 <sanujac@live.com>
2018-10-17 21:00:28 +0530

    Unary API Server Implementation

--- generate.sh
+++ generate.sh
@@ -1,4 +1,14 @@
 #!/bin.bash
+
+# Converts all .proto files into Go
 protoc --go_out=. *.proto
 
+# Converts specified proto file into gRPC in Go
 protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
+
+
+# Runs Go Server
+go run greet/greet_server/server.go
+
+# Runs Go Client
+go run greet/greet_client/client.go
