b50b549990ea5d257c5d71fe6012e693a611aa08
Sanuja91 <sanujac@live.com>
2018-10-18 11:30:54 +0530

    Server Streaming API Server Implementation

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"time"
 	"context"
 	"fmt"
 	"log"
 	"net"
+	"strconv"
 
 	"../greetpb"
 	"google.golang.org/grpc"
@@ -13,6 +15,7 @@ import (
 type server struct{}
 
 // Writes a fucntion onto a pointer
+// Unary
 func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
 	fmt.Printf("Greet function was invoked with %v\n", req)
 	firstName := req.GetGreeting().GetFirstName()
@@ -21,6 +24,22 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.G
 		Result: result,
 	}
 	return res, nil
+}
+
+// Stream Server
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+	fmt.Printf("GreetManyTimes function was invoked with %v\n",req)
+	firstName := req.GetGreeting().GetFirstName()
+	for i := 0; i < 10; i++ {
+		// strconv.Itoa converst an integer to a string
+		result := "Hello " + firstName + " number " + strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(1000 + time.Millisecond)
+	}
+	return nil
 }
 
 func main() {
